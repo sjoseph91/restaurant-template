5989f6ed07010b5310c1597c7e09f542c82437f0
Sean Joseph <sean_joseph@protonmail.com>
2022-12-06 14:30:40 -0800

    styled about section

--- src/assets/components/About.jsx
+++ src/assets/components/About.jsx
@@ -2,25 +2,31 @@ import React from "react";
 
 export default function About() {
   return (
-    <section className="container-fluid p-4 bg-white vh-100">
+    <section className="container-fluid p-4 about vh-100">
       <div className="row">
         <div className="col-lg-5 about-pic"></div>
-        <div className="col-lg-7 p-4">
-          <h2>
+        <div className="col-lg-7 p-4 d-flex flex-column justify-content-evenly align-items-stretch ">
+          <h2 className="text-center">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt,
-            illo?
+            illo? Lorem ipsum dolor,
           </h2>
           <p>
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia,
-            sit?
+            sit? sit amet consectetur adipisicing elit. Rerum, voluptas!
           </p>
           <p>
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia,
-            sit?
+            sit? sit amet consectetur adipisicing elit. Rerum, voluptas!
           </p>
+          <ul>
+            <li>An item</li>
+            <li>A second item</li>
+            <li>A third item</li>
+          </ul>
           <p>
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia,
-            sit?
+            sit? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Inventore, enim.
           </p>
         </div>
       </div>
